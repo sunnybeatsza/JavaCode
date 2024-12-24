b58c271995074619ff677806f0b039a9f61bb748
sunnybeatsza <makgerutumisho55@gmail.com>
2024-12-24 17:38:22 +0200

    Updates to the code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaCode
+
+### Welcome to Java Code!
+
+This repository contains all the basic code for a Java beginner. 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I wrote my first lines of Java Code!");
         System.out.println(3 + 3);
@@ -10,12 +10,13 @@ public class Main {
         int age = 22;
         double height = 1.75d;
         boolean isEnrolled = true;
-        char averageMark = 'A';
+        char averageMark = 0;
+
         int weight = 75;
 
         // BMI calculation = weight / height ** 2
         // Narrow TypeCasting
-        int BMI =  weight / (int) (height * height);
+        int BMI = weight / (int) (height * height);
         System.out.println(BMI);
 
         //String methods
@@ -30,7 +31,17 @@ public class Main {
         int maxNum = Math.max(5, 10);
         int minNum = Math.min(5, 10);
         int sqrtNum = (int) Math.sqrt(9);
-        int randomNum = (int) (Math.random() * 10);
+        int randomNum = (int) (Math.random() * 100);
+        if (randomNum > 50 && randomNum < 70) {
+            averageMark = 'D';
+        } else if (randomNum > 70 && randomNum < 80) {
+            averageMark = 'B';
+        } else if (randomNum > 80 ) {
+            averageMark = 'A';
+        } else {
+            averageMark = 'F';
+        }
+
 
         System.out.println("Max: " + maxNum);
         System.out.println("Min: " + minNum);
@@ -38,21 +49,31 @@ public class Main {
         System.out.println(randomNum);
 
         //Booleans Practice and Logical operators
-        if (randomNum == 5){
+        if (randomNum == 5) {
             System.out.println("The number is equal to 5!");
-        } else if (randomNum == 3){
+        } else if (randomNum == 3) {
             System.out.println("The number is equal to 3!");
-        } else{
+        } else {
             System.out.println("Our guess was incorrect!");
         }
 
         //Switch case
-        switch (averageMark){
+        System.out.println("You average grade is " + averageMark);
+        switch (averageMark) {
             case 'A':
                 System.out.println("You have a distinction!");
                 break;
             case 'B':
                 System.out.println("You almost got a distinction!");
+                break;
+            default:
+                System.out.println("Work Harder!");
+                isEnrolled = false;
+                System.out.println("You have been kicked out!");
         }
+
+        //While loop
+
+
     }
 }
